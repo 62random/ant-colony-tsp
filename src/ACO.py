030678dc85c3ea6f67c981ede92c98c6eb113751
62random <josepedropinto98@gmail.com>
2020-03-31 01:09:56 +0100

    tsp utility class ready ant colony optimization ready basic visualization

---
+++ src/ACO.py
@@ -0,0 +1,106 @@
+import numpy as np
+import random
+
+
+# Abstract class for applying the Ant Colony Optimization strategy
+# to a given problem. Some assumptions are made, so the representation
+# of the problem must meaningfully implement (at least) the functions
+# called from the self.env variable (env for environment)
+class AntColonyOptimizer():
+    # Instantiation of the optimizer
+    def __init__(self, env, n_ants=100, evap_rate=0.5, alpha=1, beta=2, substitution=False):
+        self.n_ants = n_ants
+        self.evap_rate = evap_rate 
+        self.alpha = alpha 
+        self.beta = beta
+        self.env = env
+        # Substitution -> if we can repeat steps in our solutions (we can't in TSP, for example)
+        self.substitution = substitution
+
+        self.visibility = 1/env.d 
+        self.visibility[self.visibility == np.inf] = 0
+
+    # Finding a solution in a given number of iterations or if the used evaluation metric
+    # does not improve in a given number of consecutive steps
+    def run(self, max_iter=100, start_point=0, random_start=False, convergence_steps=10):
+        
+        # Initialize pheromone distribution and solutions
+        pheromone = .1 * np.ones(self.env.step_space())
+        solutions = np.ones((self.n_ants, self.env.max_solution_size()), dtype=int)
+        metric_records = np.empty(max_iter)
+        metric_records[:] = np.nan
+
+
+        # Current best solution metric value (distance in the case of tsp, for example)
+        alltime_best_metric = self.env.initial_metric_value()
+        convergence_counter = 0
+
+        # Loop until converges or until reaches max iterations
+        for iteration in range(max_iter):
+
+            # Loops through every ant
+            for i in range(self.n_ants):
+                # Set starting point for solution
+                solutions[i, -1] = solutions[i, 0] = self.env.first_step(start_point, random_start)
+                attractiveness = self.env.attractiveness()
+
+                # Build the solution step by step
+                for j in range(self.env.solution_steps()):
+                    current_location = solutions[i, j]
+                    # Make sure we don't repeat steps in the solution
+                    if not self.substitution:
+                        attractiveness[:, current_location] = 0
+
+                    # Formula to chose next step in ACO
+                    # Each variable name represents it's role in the formula
+                    pheromone_factor = np.power(pheromone[current_location, :], self.alpha)
+                    attractiveness_factor = np.power(attractiveness[current_location, :], self.beta)
+                    product = np.multiply(pheromone_factor, attractiveness_factor)
+                    total = np.sum(product)
+                    probabilities = product/total
+
+                    # To choose the next step
+                    cum_prob = np.cumsum(probabilities)
+                    r = random.random()
+                    solutions[i, j + 1] = np.nonzero(cum_prob > r)[0][0]
+
+            # Calculating the solution evaluations
+            solution_evaluations = np.zeros(self.n_ants)
+            for i in range(self.n_ants):
+                solution_evaluations[i] = self.env.evaluate(solutions[i])
+            
+            # Pick best solution in this iteration
+            best_solution_index = np.argmin(solution_evaluations)
+            best_metric_iteration = solution_evaluations[best_solution_index]
+            best_solution_iteration = solutions[best_solution_index]
+            # If this is a new record, save the solution!
+            if best_metric_iteration < alltime_best_metric:
+                alltime_best_metric = best_metric_iteration
+                alltime_best_solution = best_solution_iteration
+                convergence_counter = 0
+            else: 
+                convergence_counter += 1
+
+            # To later plot the convergence of the algorithm
+            metric_records[iteration] = alltime_best_metric
+            
+            if convergence_counter > convergence_steps:
+                return alltime_best_solution, metric_records[np.logical_not(np.isnan(metric_records))]
+
+            # Update pheromone spreading
+            pheromone = (1 - self.evap_rate) * pheromone
+            for i in range(self.n_ants):
+                for j in range(self.env.solution_steps()):
+                    dt = self.env.pheromone_variation(solution_evaluations[i])
+                    pheromone[solutions[i, j], solutions[i, j+1]] += dt
+
+        return alltime_best_solution, metric_records[np.logical_not(np.isnan(metric_records))]
+            
+
+            
+
+
+
+
+
+
---
+++ src/TSP.py
@@ -0,0 +1,107 @@
+import numpy as np 
+from PIL import Image, ImageDraw, ImageColor
+from IPython.display import display
+import random 
+from math import sqrt
+import sys
+
+
+# Class that describes the Traveling Salesman Problem
+# Utility class including some features to support the
+# Solution of this problem with a given strategy, for example
+# Ant Colony Optimization
+class TSP:
+    def __init__(self, n_nodes, height=20, width=20):
+        self.n_nodes = n_nodes
+        self.height = height
+        self.width = width
+
+        nodes = []
+        for _ in range(n_nodes):
+            nodes.append((random.random()*width, random.random()*height))
+
+        self.nodes = np.array(nodes, dtype='float16')
+
+        d = []
+        for i in self.nodes:
+            l = []
+            for j in self.nodes:
+                l.append(sqrt((i[0] - j[0])**2 + (i[1] - j[1])**2))
+            d.append(l)
+        self.d = np.array(d, dtype='float16')
+
+    # The shape of the space of possible combinations for steps that
+    # constitute valid solutions
+    def step_space(self):
+        return self.n_nodes, self.n_nodes
+
+    # Initial (worst possible) value for metric that we want to
+    # optimize to indicate that we found a good solution
+    def initial_metric_value(self, metric='distance'):
+        if metric == 'distance':
+            return sys.float_info.max
+
+    # Returns the first step in a given solution
+    def first_step(self, start_point, random_start=False):
+        if random_start:
+            if random.random() > 0.5:
+                return start_point
+            else:
+                return random.randint(0, self.n_nodes - 1)
+        else:
+            return start_point
+
+    # Attractiveness of the possible steps
+    # in the context of approaches like Ant Colony Optimization
+    def attractiveness(self):
+        return 1/self.d
+
+    # Returns the number of steps required to construct a solution
+    def solution_steps(self):
+        return self.n_nodes - 1
+
+    # Maximum size for a valid solution to the problem
+    def max_solution_size(self):
+        return self.n_nodes + 1
+
+    # Evaluates a solution to the problem
+    # Returns the distance of the path
+    def evaluate(self, solution):
+        distance = 0
+        for j in range(self.n_nodes):
+            distance += self.d[solution[j]][solution[j + 1]]
+        distance += self.d[solution[0]][solution[-1]]
+
+        return distance
+
+    # Returns the pheromonal variation according to an evaluation
+    # Only makes sense when using Ant Colony Optimization
+    def pheromone_variation(self, evaluation):
+        return 1/evaluation
+
+    # Renders the environment and, if provided, solution paths
+    def render(self, dim=600, paths=None):
+        window_width = dim
+        window_height = int((self.height/float(self.width))*window_width)
+        image = Image.new(mode='RGBA', size=(window_width, window_height), color='white')
+
+        draw = ImageDraw.Draw(image, 'RGBA')
+
+        psize = (window_width/(self.n_nodes*5))
+        for n in self.nodes:
+            draw.ellipse([n[0]*window_width/self.width - psize, n[1]*window_height/self.height - psize, 
+                            n[0]*window_width/self.width + psize, n[1]*window_height/self.height + psize], fill='black')
+        
+        if paths:
+            colors = ['red', 'blue']
+            for j in range(len(paths)):
+                path = paths[j]
+                for i in range(len(path) - 2):
+                    p0 = self.nodes[path[i]]
+                    p1 = self.nodes[path[i+1]]
+                    draw.line([p0[0]*window_width/self.width, p0[1]*window_height/self.height,
+                            p1[0]*window_width/self.width, p1[1]*window_height/self.height],
+                            fill=colors[j % len(colors)], width=2)
+
+        del draw
+        return display(image)
